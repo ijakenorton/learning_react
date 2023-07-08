7b963f6e57da4bc37e72f69834d5559db8f4781d
Jake Norton <norja159@student.otago.ac.nz>
2023-07-08 15:23:36 +1200

    color mode switch

---
+++ game-hub/src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "../components/ColorModeSwitch";
+
+const NavBar = () => {
+  return (
+    <HStack justifyContent='space-between'padding='10px'>
+      <Image src={logo} boxSize="60px"></Image>
+      <ColorModeSwitch></ColorModeSwitch>
+    </HStack>
+  );
+};
+
+export default NavBar;
+
