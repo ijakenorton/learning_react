ed3e3ea19bdc17b691d7a8d964f2955712079fcc
Jake Norton <norja159@student.otago.ac.nz>
2023-07-08 20:04:06 +1200

    displaying platform icons

---
+++ game-hub/src/services/api-client.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+export default axios.create({
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: API_KEY
+    }
+})
