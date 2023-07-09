02da4ee6ec19d0b5bddba7b87c5bd24a53b3dde6
Jake Norton <norja159@student.otago.ac.nz>
2023-07-09 16:00:45 +1200

    added genres and spinner

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,7 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from './components/NavBar'
-import GameGrid from './components/GameGrid'
-import GenreList from './components/GenreList'
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 function App() {
   return (
     <Grid
@@ -9,12 +9,16 @@ function App() {
         base: `"nav"  "main"`,
         lg: `"nav nav"  "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" >
+        <GridItem area="aside" paddingX={5}>
           <GenreList></GenreList>
         </GridItem>
       </Show>
--- game-hub/src/components/GameCardContainer.tsx
+++ game-hub/src/components/GameCardContainer.tsx
@@ -4,10 +4,12 @@ import React, { ReactNode } from "react";
 interface Props {
   children: ReactNode;
 }
-const GameCardContainer = ({children}: Props) => {
-  return <Box width="300px"  borderRadius={10} overflow="hidden">
-    {children}
-  </Box>;
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
 };
 
 export default GameCardContainer;
